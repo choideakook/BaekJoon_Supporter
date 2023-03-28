be852b65f7df39c79508615952c06577114b005b
choideakook <shdrnrhd113@naver.com>
2023-03-28 18:22:11 +0900

    setting

---
+++ src/main/java/com/huh/BaekJoonSupporter/BaekJoonSupporterApplication.java
@@ -0,0 +1,13 @@
+package com.huh.BaekJoonSupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekJoonSupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekJoonSupporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huh/BaekJoonSupporter/BaekJoonSupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huh.BaekJoonSupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekJoonSupporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
