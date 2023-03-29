e788dc5de9c673fcae7d9f8374e0208a85acf929
PARKPARKWOO <wy9295@naver.com>
2023-03-29 14:43:41 +0900

    feat:comment entity

--- src/main/java/com/huh/BaekJoonSupporter/Comment.java
+++ src/main/java/com/huh/BaekJoonSupporter/Comment.java
@@ -1,21 +1,24 @@
 package com.huh.BaekJoonSupporter;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@Builder
+@RequiredArgsConstructor
 public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "TEXT")
     private String desc;
 
-    private LocalDateTime createDate;
+    private LocalDateTime createDate; // 생성 날짜
+
+    private LocalDateTime modifyDate; // 수정 날짜
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
