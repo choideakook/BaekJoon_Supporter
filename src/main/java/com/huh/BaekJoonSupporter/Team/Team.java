e5d849f0bb52780066e5251d8b39f461b58551ff
CatJelly <mej1406@gmail.com>
2023-03-29 09:59:58 +0900

    feat: team entity

---
+++ src/main/java/com/huh/BaekJoonSupporter/Team/Team.java
@@ -0,0 +1,28 @@
+package com.huh.BaekJoonSupporter.Team;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Team {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Long teamId; // leaderId
+
+    private String teamName;
+
+    @OneToOne
+    private Line line;
+
+    @OneToMany
+    private List<TeamRule> teamRules;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members;
+}
