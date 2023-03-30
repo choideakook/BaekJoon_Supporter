c12b4fed160a669685660f431b0114cb219fb639
PARKPARKWOO <wy9295@naver.com>
2023-03-30 22:46:58 +0900

    fix : 해결 브랜치 생성

---
+++ src/main/java/com/huh/BaekJoonSupporter/comment/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.huh.BaekJoonSupporter.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
