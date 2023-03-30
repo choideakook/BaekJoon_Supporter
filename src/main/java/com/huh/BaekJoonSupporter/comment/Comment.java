950983a66161f9404a84b64e362e3548f26d1267
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-03-30 22:51:08 +0900

    fix : 해결 브랜치 생성 (#13)

--- src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
+++ src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
@@ -1,3 +1,4 @@
+
 package com.huh.BaekJoonSupporter.comment;
 
 import com.huh.BaekJoonSupporter.board.Board;
@@ -9,13 +10,15 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(columnDefinition = "TEXT")
-    private String desc;
+    private String content;
 
     private LocalDateTime createDate; // 생성 날짜
 
@@ -27,13 +30,4 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
 
-    @Builder
-    public Comment(Long id, String desc, LocalDateTime createDate, LocalDateTime modifyDate, Member member, Board board) {
-        this.id = id;
-        this.desc = desc;
-        this.createDate = createDate;
-        this.modifyDate = modifyDate;
-        this.member = member;
-        this.board = board;
-    }
 }
