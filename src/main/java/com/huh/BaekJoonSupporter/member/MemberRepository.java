33da5e339eba04f7f9610f7dd9ea40c401afa8cc
yurrrrri <yu_ri_son@naver.com>
2023-03-30 23:29:49 +0900

    feat: MemberService

--- src/main/java/com/huh/BaekJoonSupporter/member/MemberRepository.java
+++ src/main/java/com/huh/BaekJoonSupporter/member/MemberRepository.java
@@ -2,5 +2,8 @@ package com.huh.BaekJoonSupporter.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMemberName(String name);
 }
