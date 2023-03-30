1fff88f01f9b3d1efd83154165877245843ee9d9
yurison <107273899+yurrrrri@users.noreply.github.com>
2023-03-31 08:39:08 +0900

    feat: MemberController, MemberCreateForm (#16)
    * feat(Member Entity): 필드 구현
    
    * feat(Member Entity): 필드 구현
    
    * feat(Member Entity): 필드 구현
    
    * feat: MemberService
    
    * feat:MemberController, MemberCreateForm

---
+++ src/main/java/com/huh/BaekJoonSupporter/member/MemberController.java
@@ -0,0 +1,36 @@
+package com.huh.BaekJoonSupporter.member;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup_form";
+    }
+
+    @PreAuthorize("isAnonymous()")
+    @PostMapping("/signup")
+    public String signup(@Valid MemberCreateForm memberCreateForm) {
+        memberService.create(memberCreateForm.getName(), memberCreateForm.getPassword1(), memberCreateForm.getToken());
+        return "redirect:/";
+    }
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+}
