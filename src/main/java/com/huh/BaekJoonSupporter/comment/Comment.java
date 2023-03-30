ce165f5a936d63baab10af98b093b8abf9ad1e0e
Bcoun_try <115536240+choideakook@users.noreply.github.com>
2023-03-30 21:55:23 +0900

    docs:충돌 수정

--- src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
+++ src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
@@ -1,32 +1,39 @@
 package com.huh.BaekJoonSupporter.comment;
 
-import com.huh.BaekJoonSupporter.board.Board;
-import com.huh.BaekJoonSupporter.member.Member;
-import jakarta.persistence.;
-import lombok.;
+ import com.huh.BaekJoonSupporter.board.Board;
+ import com.huh.BaekJoonSupporter.member.Member;
+ import jakarta.persistence.*;
+ import lombok.*;
 
-import java.time.LocalDateTime;
+ import java.time.LocalDateTime;
 
-@Entity
-@Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class Comment {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+ @Entity
+ @Getter
+ @NoArgsConstructor(access = AccessLevel.PROTECTED)
+ public class Comment {
+     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+     @Column(columnDefinition = "TEXT")
+     private String desc;
 
-    private LocalDateTime createDate; // 생성 날짜
+     private LocalDateTime createDate; // 생성 날짜
 
-    private LocalDateTime modifyDate; // 수정 날짜
+     private LocalDateTime modifyDate; // 수정 날짜
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Member member;
+     @ManyToOne(fetch = FetchType.LAZY)
+     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Board board;
+     @ManyToOne(fetch = FetchType.LAZY)
+     private Board board;
 
-}
+     @Builder
+     public Comment(Long id, String desc, LocalDateTime createDate, LocalDateTime modifyDate, Member member, Board board) {
+         this.id = id;
+         this.desc = desc;
+         this.createDate = createDate;
+         this.modifyDate = modifyDate;
+         this.member = member;
+         this.board = board;
+     }
+ }
