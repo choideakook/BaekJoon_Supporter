8457bc9353fa79ccc22ae86ca38515c6da08aad3
Bcoun_try <115536240+choideakook@users.noreply.github.com>
2023-03-30 21:51:03 +0900

    docs: 충돌수정

--- src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
+++ src/main/java/com/huh/BaekJoonSupporter/comment/Comment.java
@@ -2,20 +2,22 @@ package com.huh.BaekJoonSupporter.comment;
 
 import com.huh.BaekJoonSupporter.board.Board;
 import com.huh.BaekJoonSupporter.member.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.;
+import lombok.;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(columnDefinition = "TEXT")
-    private String desc;
+    private String content;
 
     private LocalDateTime createDate; // 생성 날짜
 
@@ -27,13 +29,4 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
 
-    @Builder
-    public Comment(Long id, String desc, LocalDateTime createDate, LocalDateTime modifyDate, Member member, Board board) {
-        this.id = id;
-        this.desc = desc;
-        this.createDate = createDate;
-        this.modifyDate = modifyDate;
-        this.member = member;
-        this.board = board;
-    }
 }
