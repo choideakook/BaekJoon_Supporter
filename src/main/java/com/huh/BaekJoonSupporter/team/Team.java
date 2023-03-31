d1abc4aa404d4a21e1bfc3341241ab11e7450959
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-31 13:29:07 +0900

    fix: line dir delete (#23)
    * feat: team service enhancement
    
    * feat: team service enhancement(2)
    
    * fix: line dir delete

--- src/main/java/com/huh/BaekJoonSupporter/team/Team.java
+++ src/main/java/com/huh/BaekJoonSupporter/team/Team.java
@@ -4,10 +4,7 @@ import com.huh.BaekJoonSupporter.line.Line;
 import com.huh.BaekJoonSupporter.member.Member;
 import com.huh.BaekJoonSupporter.teamrule.TeamRule;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
@@ -16,11 +13,11 @@ import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
-@Getter
-@Setter
 @Entity
+@Getter
 @Builder
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class Team {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,13 +36,13 @@ public class Team {
     private List<Member> members = new ArrayList<>();
 
     private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 
     @Builder
-    public Team(Long id, Long leaderId, String teamName, Line line, Member leader, LocalDateTime createDate) {
+    public Team(Long id, Long leaderId, String teamName, Line line, LocalDateTime createDate) {
         this.leaderId = leaderId;
         this.teamName = teamName;
         this.line = line;
-        members.add(leader);
         this.createDate = createDate;
     }
 }
