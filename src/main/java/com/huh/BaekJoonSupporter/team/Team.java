5acf3f3614d9d9abb9b46bf5557c65dd820f61ca
CatJelly <mej1406@gmail.com>
2023-03-31 10:18:30 +0900

    feat: team service enhancement

--- src/main/java/com/huh/BaekJoonSupporter/team/Team.java
+++ src/main/java/com/huh/BaekJoonSupporter/team/Team.java
@@ -8,7 +8,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +19,7 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 @Setter
 @Entity
+@Builder
 @NoArgsConstructor(access = PROTECTED)
 public class Team {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,16 +33,19 @@ public class Team {
     private Line line;
 
     @OneToMany
-    private List<TeamRule> teamRules;
+    private List<TeamRule> teamRules = new ArrayList<>();
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
+    private LocalDateTime createDate;
+
     @Builder
-    public Team(Long leaderId, String teamName, Line line, Member leader) {
+    public Team(Long id, Long leaderId, String teamName, Line line, Member leader, LocalDateTime createDate) {
         this.leaderId = leaderId;
         this.teamName = teamName;
         this.line = line;
         members.add(leader);
+        this.createDate = createDate;
     }
 }
--- src/main/java/com/huh/BaekJoonSupporter/team/TeamService.java
+++ src/main/java/com/huh/BaekJoonSupporter/team/TeamService.java
@@ -1,27 +1,46 @@
 package com.huh.BaekJoonSupporter.team;
 
+import com.huh.BaekJoonSupporter.DataNotFoundException;
+import com.huh.BaekJoonSupporter.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.sound.sampled.Line;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class TeamService {
     private final TeamRepository teamRepository;
 
-    public Team create() {
-        return null;
+    public Team create(Long leaderId, String teamName, Line line, Member leader, LocalDateTime createDate) {
+        Team team = Team.builder()
+                .leaderId(leaderId)
+                .teamName(teamName)
+                .line(line)
+                .leader(leader)
+                .createDate(createDate)
+                .build();
+        this.teamRepository.save(team);
+        return team;
     }
 
-    public Team getTeam() {
-        return null;
+    public Team getTeam(Long id) {
+        Optional<Team> team = this.teamRepository.findById(id);
+        if(team.isPresent()) {
+            return team.get();
+        } else {
+            throw new DataNotFoundException("comment not found");
+        }
     }
 
-    public void modify() {
+    public void modify(Team team) {
 
     }
 
-    public void delete() {
-
+    public void delete(Team team) {
+        this.teamRepository.delete(team);
     }
 }
 
