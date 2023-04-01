bf493bdb48628d5b2a8fb9c79f2bbffcdb388f3f
choideakook <shdrnrhd113@naver.com>
2023-04-01 19:08:46 +0900

    feat: 개발준비 완료

--- src/main/java/com/huh/BaekJoonSupporter/board/BoardController.java
+++ src/main/java/com/huh/BaekJoonSupporter/board/BoardController.java
@@ -2,6 +2,7 @@ package com.huh.BaekJoonSupporter.board;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,8 @@ public class BoardController {
 
     private final BoardService boardService;
 
-
+    @GetMapping("/list")
+    public String showList() {
+        return "/board/boardList";
+    }
 }
