b517cc561b5e50276426c1f2db330b757120a935
CheorHyeon <126079049+CheorHyeon@users.noreply.github.com>
2023-04-10 17:52:50 +0900

    feat : teamrule 엔티티 수정 및 create 테스트 (#64)

--- src/main/java/com/huh/BaekJoonSupporter/teamrule/TeamRule.java
+++ src/main/java/com/huh/BaekJoonSupporter/teamrule/TeamRule.java
@@ -14,8 +14,23 @@ public class TeamRule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String ruleName; // Map < k 문제 푼수면 : v 문제수  > 1. 문제 푼수(solvcedCount), 2. 난이도별 문제 푼 수
+
+    // 해시 사용 대비 남겨두기
+//    @ElementCollection // 해시맵 필드 선언
+//    @CollectionTable(name="team_rule_table") //DB에서 사용할 테이블 이름
+//    @MapKeyColumn(name="attractiveTargetTypeCode") // 해시맵의 키로 사용할 필드
+//    @Column(name="targetSize") // 해시맵의 값으로 사용될 필드명
+//    @Builder.Default
+//    private Map<String, Long> ruleName = new HashMap<>();
+
+// 목표 설정 : 난이도 무관 문제풀이 / 난이도별 문제풀이
+    private String target;
+// 난이도 설정
+    private String difficulty;
+// 목표 문제 풀이 개수
+    private Long targetNumber;
     @ManyToOne
+    @ToString.Exclude
     private Team team;
 }
 
