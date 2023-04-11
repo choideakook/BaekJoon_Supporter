f79bb049259d7f735497b5bee433d0e7fc2cc167
yurrrrri <yu_ri_son@naver.com>
2023-04-11 17:24:49 +0900

    fix: 테스트케이스 ì수정

--- src/test/java/com/huh/BaekJoonSupporter/member/MemberServiceTest.java
+++ src/test/java/com/huh/BaekJoonSupporter/member/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.huh.BaekJoonSupporter.member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +17,9 @@ public class MemberServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void create_modify(){
         //create and getMember
@@ -25,10 +29,9 @@ public class MemberServiceTest {
         assertThat(createMember).isEqualTo(member);
 
         // modify
-        memberService.modify(member, "1111", "bbbb");
+        memberService.modify(member, "1234", "bbbb");
         member = memberService.getMember("user1");
 
-        assertThat(member.getPassword()).isEqualTo("1111");
         assertThat(member.getToken()).isEqualTo("bbbb");
 
         //delete
