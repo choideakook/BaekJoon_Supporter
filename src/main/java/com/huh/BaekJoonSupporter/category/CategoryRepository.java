9c1069658d111dcfcf80d5ae0be703c304c439f0
choideakook <shdrnrhd113@naver.com>
2023-04-14 21:42:57 +0900

    feat: 게시물 생성에 카테고리 선택창 추가

--- src/main/java/com/huh/BaekJoonSupporter/category/CategoryRepository.java
+++ src/main/java/com/huh/BaekJoonSupporter/category/CategoryRepository.java
@@ -2,5 +2,9 @@ package com.huh.BaekJoonSupporter.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
 }
