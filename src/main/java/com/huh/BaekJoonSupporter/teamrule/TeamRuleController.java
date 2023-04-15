438c9a7f6f8e72d8f8a58c53de11af53ca638ebe
깃ChertHyeon <r4560798@naver.com>
2023-04-15 21:46:29 +0900

    feat : TeamRule JS도입, 컨트롤러 POST추가(테스트 진전)

--- src/main/java/com/huh/BaekJoonSupporter/teamrule/TeamRuleController.java
+++ src/main/java/com/huh/BaekJoonSupporter/teamrule/TeamRuleController.java
@@ -1,10 +1,16 @@
 package com.huh.BaekJoonSupporter.teamrule;
 
-import com.huh.BaekJoonSupporter.team.TeamService;
+import com.huh.BaekJoonSupporter.member.MemberService;
+import com.huh.BaekJoonSupporter.team.Team;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +19,7 @@ public class TeamRuleController {
 
     private final TeamRuleService teamRuleService;
 
-    private final TeamService teamService;
+    private final MemberService memberService;
 
     @GetMapping("/list")
     public String showList()  {
@@ -21,24 +27,30 @@ public class TeamRuleController {
     }
 
     @GetMapping("/create")
-    public String showCreate() {
+    public String Create() {
         return "teamrule/create";
     }
 
+    @PostMapping("/create")
+    @ResponseBody
+    //테스트 못해봤슴다
+    public String Create(@Valid TeamRuleCreateForm createForm, Principal principal) {
+        Team team = memberService.getMember(principal.getName()).getTeam();
+        TeamRule teamRule = null;
+        // 방식(난이도 무관/유관) 선택 전에 난이도 먼저 고른 상태에서, 무관을 할 경우 난이도 default로 변경
+        if(createForm.getTarget().equals("무관") && !createForm.getDifficulty().equals("default")) {
+            teamRule = teamRuleService.create(team, createForm.getTarget(), "default", createForm.getTargetNumber());
+        }
+        else {
+            teamRule = teamRuleService.create(team, createForm.getTarget(), createForm.getDifficulty(), createForm.getTargetNumber());
+        }
+        // 실패시 메인
+        if(teamRule ==null)
+            return "실패";
 
+        // 규칙 생성 시 목록으로
+        return "성공";
 
-//    @PostMapping("/create")
-//    public String ProcessCreate(@Valid CreateForm createForm) {
-//        Team team = teamService.create(5L,"테스트팀", null, null);
-//        TeamRule teamRule = teamRuleService.create(team, createForm.targetTypeCode, createForm.targetNumber);
-//
-//        // 실패시 메인
-//        if(teamRule ==null)
-//            return "redirect:/test";
-//
-//        // 규칙 생성 시 목록으로
-//        return "redirect:teamrule/list";
-//
-//    }
+    }
 
 }
