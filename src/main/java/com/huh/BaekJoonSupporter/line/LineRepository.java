56340ab297f6fe824ac928d3c5f0b41339e215a7
김동우 <nanaukim@naver.com>
2023-04-17 17:58:40 +0900

    feat: findByToken메서드 작성

--- src/main/java/com/huh/BaekJoonSupporter/line/LineRepository.java
+++ src/main/java/com/huh/BaekJoonSupporter/line/LineRepository.java
@@ -2,6 +2,9 @@ package com.huh.BaekJoonSupporter.line;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LineRepository extends JpaRepository<Line, Long> {
 
+    Optional<Line> findByToken(String token);
 }
